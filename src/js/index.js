90b95ff29fe39b57d2cf9ab24f21ceac7846b84d
Shovan Chatterjee <shovanc.web@gmail.com>
2018-05-10 00:43:37 +0530

    🔑 Setup API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
-// Global app controller
-console.log("The app is running");
+import axios from "axios";
+
+async function getResults(query) {
+  const proxy = "https://cors-anywhere.herokuapp.com/";
+  const key = "a29e6f0d970c58f6b815fcf932d8b209";
+  const url = "http://food2fork.com/api/search";
+
+  try {
+    const res = await axios(`${proxy}${url}?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getResults("cake");
