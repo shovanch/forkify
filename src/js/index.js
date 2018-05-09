9b6aa4a4e4d37e6fce37a1c91bf908aa21909d53
Shovan Chatterjee <shovanc.web@gmail.com>
2018-05-10 00:57:31 +0530

    📦 Setup search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,5 @@
-import axios from "axios";
+import Search from "./models/Search";
 
-async function getResults(query) {
-  const proxy = "https://cors-anywhere.herokuapp.com/";
-  const key = "a29e6f0d970c58f6b815fcf932d8b209";
-  const url = "http://food2fork.com/api/search";
-
-  try {
-    const res = await axios(`${proxy}${url}?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-
-getResults("cake");
+const search = new Search("pizza");
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = "a29e6f0d970c58f6b815fcf932d8b209";
+    const url = "http://food2fork.com/api/search";
+
+    try {
+      const res = await axios(`${proxy}${url}?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
